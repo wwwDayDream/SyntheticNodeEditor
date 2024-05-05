829216889edd7b37dbf35b4e029dddd696144acf
wwwDayDream <daymonforeman14@gmail.com>
2024-05-05 14:54:54 -0500

    Initial

---
+++ NodeEditor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace NodeEditor;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
+public class ConnectorViewModel
+{
+    public string Title { get; set; }
+}
+public class NodeViewModel
+{
+    public string Title { get; set; }
+    public ObservableCollection<ConnectorViewModel> Input { get; set; } = new ObservableCollection<ConnectorViewModel>();
+    public ObservableCollection<ConnectorViewModel> Output { get; set; } = new ObservableCollection<ConnectorViewModel>();
+
+}
+
+public class EditorViewModel
+{
+    public ObservableCollection<NodeViewModel> Nodes { get; } = new ObservableCollection<NodeViewModel>();
+
+    public EditorViewModel()
+    {
+        Nodes.Add(new NodeViewModel { Title = "Welcome", Input = {new ConnectorViewModel() { Title = "Test" }}});
+        Nodes.Add(new NodeViewModel { Title = "Welcome", Output = {new ConnectorViewModel() { Title = "Test" }}});
+
+    }
+}
